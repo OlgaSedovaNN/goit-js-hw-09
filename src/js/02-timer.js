bb270d26d1534e9607849532e418c5d53f7c80c1
OlgaSedovaNN <olga_04021991@mail.ru>
2022-01-25 19:20:57 +0300

    готово 1 и 2 задания

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,44 +1,69 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// const input = document.querySelector('#datetime-picker')
 
 const startBtn = document.querySelector('button')
-const refs = {
-    sec: document.querySelector('[data-seconds]'),
-    min: document.querySelector('[data-minutes]'),
-    hours:document.querySelector('[data-hours]'),
-    days:document.querySelector('[data-days]')
-}
+const input = document.querySelector('#datetime-picker')
+
         
-    
 startBtn.setAttribute("disabled", true)
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
-  minuteIncrement: 1,
+    minuteIncrement: 1,
+  
     onClose(selectedDates) {
         const balanceMs = selectedDates[0] - options.defaultDate
-        if (balanceMs <= 0) {
-         window.alert("Please choose a date in the future")
+     if (balanceMs <= 0) {
+  
+         Notify.warning("Please choose a date in the future");
         } else {
-            startBtn.removeAttribute("disabled");
-            
-      }    
-  },
+         startBtn.removeAttribute("disabled");
+         const initialTime = selectedDates[0];
+         
+         startBtn.addEventListener('click', () => { 
+             const timer = new Timer();
+             timer.start(initialTime)
+         });
+        } 
+    }    
 };
 
 flatpickr("#datetime-picker", options)
-startBtn.addEventListener('click', updateTimer())
 
-function updateTimer() {
-    setInterval(() => {
-        const balanceDate = convertMs(balanceMs)
-       
-    }, 1000)
+class Timer {
+    constructor() {
+        this.intervalId = null;
+        this.refs = {
+    sec: document.querySelector('[data-seconds]'),
+    min: document.querySelector('[data-minutes]'),
+    hours:document.querySelector('[data-hours]'),
+    days:document.querySelector('[data-days]')
+    }
 }
+
+    start(time) {
+        startBtn.setAttribute("disabled", true)
+        this.intervalId = setInterval(() => {
+            const deltaTime = time - new Date()
+            const timerTime = convertMs(deltaTime)
+            this.addLeadingZero(timerTime)
+        }, 1000)
+    }
+
+addLeadingZero({ days, hours, minutes, seconds }) {
+    this.refs.sec.textContent = String(seconds).padStart(2,'0');
+    this.refs.min.textContent = String(minutes).padStart(2,'0');
+    this.refs.hours.textContent = String(hours).padStart(2,'0');
+    this.refs.days.textContent = String(days).padStart(2,'0');
+ 
+    }
+
+}
+
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -58,3 +83,5 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+
+
